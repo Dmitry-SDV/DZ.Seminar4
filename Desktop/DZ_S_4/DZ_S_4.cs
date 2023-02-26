83c8dc99358a9d3ec7830bce16018d1873c48606
Dmitry <124237025+Dmitry-SDV@users.noreply.github.com>
2023-02-26 16:43:37 +0500

    Сохранение перед отправкой

---
+++ Desktop/DZ_S_4/DZ_S_4.cs
@@ -0,0 +1,51 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+/*Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());// Вводим числа А и В
+int count = A;
+for (int i = 1; i <= B -1; i++)// Создаем цикл, переменная i, которая равна всем значениям от 1 до В
+{
+    count = count * A; // сумма = сумма + я;
+}
+Console.Write(count);// Вывод числа А в степени В
+*/
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*Console.Write("Введите число А(Number): ");
+int stringNumber = Convert.ToInt32(Console.ReadLine());// ВВели число
+static extern
+int GetSum (int Number);// Создали метод
+int S = 0;
+for (int i = 0; S = S + GetSum (stringNumber (i)); i++)// Number Просит имя метода - создал метод, все равно ошибка
+{
+
+}
+return stringNumber;// Не пойму что этот оператор возвращает и куда ...
+
+
+Console.WriteLine($"Сумма цифр в числе: {GetSum (stringNumber)}");
+*/
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+/*void FillArray(int[] Array);
+{
+    int [] Array = new int [8];
+    int Length = Array.Length;
+    int i;
+    for (i=0; i < 8; i++)
+    {
+        Array [i] = new Random().Next(1, 100);
+        Console.WriteLine(Array(i));// Просят указать имя метода, я тут метод не указываю ...
+    }
+    return FillArray
+}
+
+*/
+
+
+
+ 
